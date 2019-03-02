ab620c00c7aa11aad490e2d7cf80724cb97bc2ec
ADIL NAIMI <adilnm23@hotmail.com>
2019-03-02 08:23:54 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -4,9 +4,13 @@ class Shoe
   BRANDS=[]
   def initialize(brand)
     @brand = brand
+<<<<<<< HEAD
         if !BRANDS.include?(brand)
     BRANDS<< brand
   end
+=======
+    BRANDS<< brand
+>>>>>>> d105ac94adae2e6bedba5dcd0cbb88b9b7bb2786
   end
 
   def cobble
@@ -14,5 +18,12 @@ class Shoe
     puts "Your shoe is as good as new!"
   end
   
+<<<<<<< HEAD
+=======
+  def brand=(brand)
+    @brand=brand
+    BRANDS<< brand
+  end
+>>>>>>> d105ac94adae2e6bedba5dcd0cbb88b9b7bb2786
 
 end
